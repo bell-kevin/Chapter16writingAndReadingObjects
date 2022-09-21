50436ebe7457380f9c1a9b641f81671e094c8c37
Kevin Bell <bellKevin@pm.me>
2022-09-20 18:55:29 -0600

    Update Ch16writingAndReadingObjects.java

--- ch16writingAndReadingObjects/src/ch16writingandreadingobjects/Ch16writingAndReadingObjects.java
+++ ch16writingAndReadingObjects/src/ch16writingandreadingobjects/Ch16writingAndReadingObjects.java
@@ -14,5 +14,8 @@ public class Ch16writingAndReadingObjects {
 
     public static void main(String[] args) {
         System.out.println("Chapter 16 Objects in Files by Kevin Bell \n");
+        String[] names = {"Caleb", "Izumi", "Mary", "Usha"};
+        StudentList studentList = new StudentList(names);
+        studentList.display();
     } // end main method
 } // end Ch16writingAndReadingObjects class
