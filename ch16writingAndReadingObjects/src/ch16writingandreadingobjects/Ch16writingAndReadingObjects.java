47a6e959f670c0e142e37b998eda5f70456e16c0
Kevin Bell <bellKevin@pm.me>
2022-09-20 20:04:58 -0600

    Update Ch16writingAndReadingObjects.java

--- ch16writingAndReadingObjects/src/ch16writingandreadingobjects/Ch16writingAndReadingObjects.java
+++ ch16writingAndReadingObjects/src/ch16writingandreadingobjects/Ch16writingAndReadingObjects.java
@@ -16,16 +16,20 @@ public class Ch16writingAndReadingObjects {
         System.out.println("Chapter 16 Objects in Files by Kevin Bell \n");
         String[] names = {"Caleb", "Izumi", "Mary", "Usha"};
         StudentList studentList = new StudentList(names);
+        System.out.println("List 1: ");
         studentList.display();
 //******************************************************************************
         // write object
         Scanner computerKeyboardInput = new Scanner(System.in);
         System.out.print("Enter fileName: ");
+        String fileName = computerKeyboardInput.nextLine();
         try (ObjectOutputStream fileOut
-                = new ObjectOutputStream(new FileOutputStream(
-                        computerKeyboardInput.nextLine()))) {
-            studentList = new StudentList(names);
+                = new ObjectOutputStream(new FileOutputStream(fileName))) {
             fileOut.writeObject(studentList);
+            fileOut.reset();
+            studentList.addStudent("Anna");
+            studentList.addStudent("James");
+            studentList.addStudent("Noah");
         } // end try, and close fileOut automatically
         catch (Exception e) {
             System.out.println(e.getClass());
